0f497c9a1c280c281a9feda806ed428553bfb959
pyaephyokyaw-coding <pyaephyokyaw.dev@gmail.com>
2025-03-02 07:40:16 +0630

    Global object

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+// globalThis.setTimeout(
+//     function(){
+//         console.log("hello world")
+//     }, 3000
+// )
+
+// let intervel = globalThis.setInterval(
+//     function () {
+//         console.log('hello world');
+//     }, 1000
+// )
+
+// globalThis.setTimeout(
+//     function(){
+//         clearInterval(intervel);
+//     }, 4000
+// )
+
+console.log(__dirname);
+console.log(__filename);
