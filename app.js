3bd1e4fb3fc1959936b16acae0a6ab40e275a687
pyaephyokyaw-coding <pyaephyokyaw.dev@gmail.com>
2025-03-11 06:06:27 +0630

    Ch-3-Ep-8-ep-19-dynamic-title

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const port = 9000;
 const express = require('express');
+const { times } = require('lodash');
 const app = express();
 
 app.set('views', './chapter2');
@@ -14,36 +15,46 @@ app.get('/', (req, res) => {
     // res.sendFile('./chapter2/home.html', {root : __dirname})
 
     let blogs = [
-        {title: 'title1', page: 'Ch-3-Ep-5-ep-16-ejs-view-engine Ch-3-Ep-6-ep-17-ejs-pass-data-and-render-dynamic-contents'},
-        {title: 'title2', page: 'page2'},
-        {title: 'title3', page: 'page3'}
+        { title: 'title1', page: 'Ch-3-Ep-5-ep-16-ejs-view-engine Ch-3-Ep-6-ep-17-ejs-pass-data-and-render-dynamic-contents' },
+        { title: 'title2', page: 'page2' },
+        { title: 'title3', page: 'page3' }
     ]
 
     res.render('home', {
         // name: 'Personal Website',
         // type: 'website'
 
-        blogs
+        blogs,
+        title: 'Home'
     });
 })
 
 app.get('/contact', (req, res) => {
     // res.sendFile('./chapter2/contact.html', {root : __dirname})
-    res.render('contact');
+    res.render('contact', {
+        title: 'Contact'
+    }
+    );
 })
 
 app.get('/about', (req, res) => {
     // res.sendFile('./chapter2/About.html', {root : __dirname})
-    res.render('about')
+    res.render('about', {
+        title: 'About'
+    })
 })
 
 app.get('/about-us', (req, res) => {
     // res.redirect('/about')
-    res.render('about')
+    res.render('about', {
+        title: 'About-us'
+    })
 })
 
 app.use((req, res) => {
-    res.statusCode=404;
+    res.statusCode = 404;
     // res.sendFile('./chapter2/404.html', {root : __dirname})
-    res.render('404')
+    res.render('404', {
+        title: '404'
+    })
 })
